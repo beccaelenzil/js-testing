f8f2017f696d6507d6dce44157be96a0cf46dca1
beccaelenzil <beccaelenzil@gmail.com>
2019-11-27 10:46:30 -0800

    up to date

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,26 +1,53 @@
-const letters = "abcdefghijklmnopqrstuvwxyz"
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const lettersArray = letters.split('');
+
+
+/*
+// using a forEach 
+lettersArray.forEach(letter=>lettersObj[letter] = false)
+*/
 
 const isPangram = function isPangram(text) {
-  // turn strings into arrays
-  const lettersArray = letters.split('');
-  const textArray = text.split('');
-
-  //loop through all the letters in the alphabet
+  //-- Using a Hash
+  const lettersObj = {};
   for (let i in lettersArray){
-
-    // look for each letter in the text
-    let found = textArray.find(function(letter) {
-      return letter === lettersArray[i];
-    });
-
-    // return false if you didn't find it
-    if (found === undefined){
-      return false;
-    }
+    lettersObj[lettersArray[i]] = false;
   }
 
-  //otherwise return true
-  return true;
+  const textArray = text.split('');
+  
+  //turn letter objects to true if they're in text
+  for (let i in textArray){
+    lettersObj[textArray[i]] = true
+  }
+  console.log(lettersObj)
+
+  //check if every value is true
+  const result = Object.values(lettersObj).every(value => value === true)
+
+  return result
+
+  //--Brute Force--
+  // // turn strings into arrays
+  // const lettersArray = letters.split('');
+  // const textArray = text.split('');
+
+  // //loop through all the letters in the alphabet
+  // for (let i in lettersArray){
+
+  //   // look for each letter in the text
+  //   let found = textArray.find(function(letter) {
+  //     return letter === lettersArray[i];
+  //   });
+
+  //   // return false if you didn't find it
+  //   if (found === undefined){
+  //     return false;
+  //   }
+  // }
+
+  // //otherwise return true
+  // return true;
 };
 
 module.exports = isPangram;
